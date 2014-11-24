3ab6ddfc4ecc053dc4d385db8829b99d550f1020
alexpchin <alex@womapp.com>
2014-11-24 14:20:23 +0000

    Refactoring

--- one_million.rb
+++ one_million.rb
@@ -1,20 +1,82 @@
-class Fixnum
+class Word
 
-  # Method used to print out words (1..1000000).each { |n| puts n.in_words }
-  # If no value is returned by in_a_word, then 
-  def in_words
-    in_a_word || in_many_words if know_in_words?
-  end
+  attr_reader :integer, :word, :digits, :magnitude
 
-  def digits
-    to_s.length
-  end
-
-  def multiple_of?(x)
-    self % x == 0
+  def initialize(integer)
+    @integer    = integer                 # 923454
+    @digits     = integer.to_s.length     # 6
+    @magnitude  = order_of_magnitude      # 100000
+    @word       = in_words                # nine hundred and twenty three thousand four hundred and fifty four
   end
 
   private
+
+  # Recursive method to print out word.
+  # If the number is in range (1 - 1000000):
+  # - Check if number is a unique string, in_a_word
+  # - If not, then calculate string of words, in_many_words
+  def in_words
+    in_a_word || in_many_words if in_range?
+  end
+
+  # If the number is a unique value, take directly from the WORDS hash
+  def in_a_word
+    WORDS[integer]
+  end
+
+  # Is the number a multiple of a magnitude?
+  def in_many_words
+    multiple_of_magnitude? ? multiple_of_magnitude_in_words : long_number_in_words
+  end
+
+  # Integer % magnitude
+  def multiple_of_magnitude?
+    integer % magnitude == 0
+  end
+
+  def in_range?
+    integer > LOWER_BOUND && integer <= UPPER_BOUND
+  end
+
+  # Order of magnitude followed but the magnitude word.
+  def multiple_of_magnitude_in_words
+    [(integer / magnitude).in_words, magnitude_word].join(' ')
+  end
+
+  # 
+  def long_number_in_words
+puts " "
+puts "integer: #{integer}"
+puts "digits: #{digits}"
+puts "magnitude: #{magnitude}"
+# puts magnitude_part_and_remainder
+puts integer.divmod(magnitude).class
+    magnitude_part_and_remainder.map(&:in_words).join(magnitude_separator)
+  end
+
+  # Returns an array containing the order of magnitude and remainder.
+  def magnitude_part_and_remainder
+    quotient, remainder = integer.divmod(magnitude)
+    [magnitude * quotient, remainder]
+  end
+
+  def magnitude_word
+    MAGNITUDE[magnitude]
+  end
+
+  # Calculate the order of magnitude, e.g. 
+  def order_of_magnitude
+    # return 1000 if (4..6).include?(digits)
+    # 10 ** (digits - 1)
+    10 ** digits - 1
+  end
+
+  def magnitude_separator
+    (remainder(magnitude) < 100 && magnitude > 10) ? ' and ' : ' '
+  end
+
+  LOWER_BOUND = 0
+  UPPER_BOUND = 1000000
 
   WORDS = {
     1  => "one",
@@ -46,49 +108,10 @@ class Fixnum
     90 => "ninety"
   }
 
-  MAGNITUDE_WORD = {
+  MAGNITUDE = {
     100     => "hundred",
     1000    => "thousand",
     1000000 => "million"
   }
-
-  # 1.in_a_word passes 1 as self to words.
-  def in_a_word
-    WORDS[self]
-  end
-
-  def in_many_words
-    multiple_of?(magnitude) ? round_number_in_words : long_number_in_words
-  end
-
-  def know_in_words?
-    self > 0 && self <= 1000000
-  end
-
-  def round_number_in_words
-    [(self / magnitude).in_words, magnitude_word].join(' ')
-  end
-
-  def long_number_in_words
-    magnitude_part_and_remainder.map(&:in_words).join(magnitude_separator)
-  end
-
-  def magnitude_part_and_remainder
-    quotient, remainder = divmod(magnitude)
-    [magnitude * quotient, remainder]
-  end
-
-  def magnitude
-    return 1000 if (4..6).include?(digits)
-    10 ** (digits - 1)
-  end
-
-  def magnitude_word
-    MAGNITUDE_WORD[magnitude]
-  end
-
-  def magnitude_separator
-    (remainder(magnitude) < 100 && magnitude > 10) ? ' and ' : ' '
-  end
 
 end
